8ffb8858304f74760f2ec467d7a61dc9e3b66c48
Scott Roberson <srkazan23@gmail.com>
2020-02-05 16:51:56 -0600

    hashed passwrod and insterted user & password into db

--- server/auth/index.js
+++ server/auth/index.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const Joi = require('@hapi/joi');
+const bcrypt = require('bcryptjs');
+
+const db = require('../db/connection');
+const users = db.get('users');
+users.createIndex('username', {
+  unique: true
+});
 
 const router = express.Router();
 
 const schema = Joi.object({
-  username: Joi.string().regex(/(^[a-zA-Z0-9_]*$)/).min(2).max(30)
-    .required(),
+  username: Joi.string().regex(/(^[a-zA-Z0-9_]*$)/).min(2).max(30).required(),
 
   password: Joi.string().min(10).required()
-
 })
-
 
 router.get('/', (req, res) => {
   res.json({
@@ -18,12 +22,36 @@ router.get('/', (req, res) => {
   });
 })
 
-router.post('/signup', (req, res) => {
+router.post('/signup', (req, res, next) => {
+  const result = schema.validate(req.body);
+  if (result.error === undefined) {
+    users.findOne({
+      username: req.body.username
+    }).then(user => {
+      //if user is undefined,username is not in the db,otherwise, duplicate user found
+      if (user) {
+        //there is already user in db with username...
+        //respond with error!
 
-
-  const result = schema.validate(req.body)
-  res.json(result);
-
+        const error = new Error('That username is already taken. Please choose another one');
+        next(error);
+      } else {
+        //hash the password
+        bcrypt.hash(req.body.password, 12).then(hashedPassword => {
+          //insert the user with the hashed password
+          const newUser = {
+            username: req.body.username,
+            password: hashedPassword
+          };
+          users.insert(newUser).then(insertedUser => {
+            res.json(insertedUser);
+          });
+        });
+      }
+    });
+  } else {
+    next(result.error);
+  }
 })
 
 module.exports = router;
--- server/index.js
+++ server/index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const volleyball = require('volleyball');
 
+
 const app = express();
 
-const auth = require('./auth/index.js');
+const auth = require('./auth/index');
 
 app.use(volleyball);
 app.use(express.json());
-
 
 app.get('/', (req, res) => {
   res.json({
@@ -17,7 +17,25 @@ app.get('/', (req, res) => {
 
 app.use('/auth', auth);
 
+
+function errorHandler(err, req, res, next) {
+  res.status(res.statusCode || 500);
+  res.json({
+    message: err.message,
+    stack: err.stack
+  });
+}
+
+function notFound(req, res, next) {
+  res.status(404);
+  const error = new Error('Not Found-' + req.originalUrl);
+  next(error);
+}
+
+app.use(errorHandler);
+app.use(notFound);
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
-})
+});
