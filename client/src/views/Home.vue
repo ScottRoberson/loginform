a701cae985f976adf6c4a53accde5f9c4d31b5c0
Scott Roberson <srkazan23@gmail.com>
2020-02-05 19:56:43 -0600

    started front end added vue, Home & Signup component

---
+++ client/src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home text-center">
+    <div class="jumbotron">
+  <h1 class="display-3">Super Awesome Site</h1>
+  <p class="lead">
+    Learning Auth is fun!
+  </p>
+  <hr class="my-4">
+  <p>Signup to our awesome site </p>
+  <p class="lead">
+    <router-link
+    class="btn btn-primary btn-lg"
+     :to="{name:'signup'}"
+     role="button">Sign up</router-link>
+  </p>
+</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+};
+</script>
--- server/auth/index.js
+++ server/auth/index.js
@@ -12,8 +12,7 @@ const router = express.Router();
 
 const schema = Joi.object({
   username: Joi.string().regex(/(^[a-zA-Z0-9_]*$)/).min(2).max(30).required(),
-
-  password: Joi.string().min(10).required()
+  password: Joi.string().trim().min(10).required()
 })
 
 router.get('/', (req, res) => {
@@ -32,18 +31,18 @@ router.post('/signup', (req, res, next) => {
       if (user) {
         //there is already user in db with username...
         //respond with error!
-
         const error = new Error('That username is already taken. Please choose another one');
         next(error);
       } else {
         //hash the password
-        bcrypt.hash(req.body.password, 12).then(hashedPassword => {
+        bcrypt.hash(req.body.password.trim(), 12).then(hashedPassword => {
           //insert the user with the hashed password
           const newUser = {
             username: req.body.username,
             password: hashedPassword
           };
           users.insert(newUser).then(insertedUser => {
+            delete insertedUser.password;
             res.json(insertedUser);
           });
         });
