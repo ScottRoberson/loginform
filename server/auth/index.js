d34374e9ba5128d91f761f433109fa01023eb5ac
Scott Roberson <srkazan23@gmail.com>
2020-02-09 19:48:40 -0600

    added jwt,signed it and got response back from server

--- server/auth/index.js
+++ server/auth/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const Joi = require('@hapi/joi');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+require('dotenv').config();
 
 const db = require('../db/connection');
 const users = db.get('users');
@@ -50,8 +53,55 @@ router.post('/signup', (req, res, next) => {
       }
     });
   } else {
-    res.status(406);
+    res.status(422);
     next(result.error);
+  }
+});
+
+function respondError422(res, next) {
+  res.status(422);
+  const error = new Error('Unable to login.');
+  next(error);
+}
+
+router.post('/login', (req, res, next) => {
+  const result = schema.validate(req.body);
+  if (result.error === undefined) {
+    users.findOne({
+      username: req.body.username
+    }).then(user => {
+      if (user) {
+        bcrypt
+          .compare(req.body.password, user.password)
+          .then(result => {
+            if (result) {
+              //They sent the correct password!!!
+              const payload = {
+                _id: user._id,
+                username: user.username
+              };
+              jwt.sign(payload, process.env.TOKEN_SECRET, {
+                expiresIn: '1d'
+              }, (err, token) => {
+                if (err) {
+                  respondError422(res, next);
+                } else {
+                  res.json({
+                    token
+                  });
+                }
+              });
+            } else {
+              respondError422(res, next);
+            }
+          })
+      } else {
+        respondError422(res, next);
+      }
+    });
+  } else {
+    respondError422(res, next);
+
   }
 })
 
